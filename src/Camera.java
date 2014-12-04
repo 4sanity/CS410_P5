b91c032cbafa3ae4a73a0feedb864aaf60c7e0e3
Tragdor <sam.bruns0@gmail.com>
2014-12-04 09:57:19 -0700

    edited

--- src/Camera.java
+++ src/Camera.java
@@ -1,4 +1,29 @@
-
 public class Camera {
-
+	String name;
+	double prpx;
+	double prpy;
+	double prpz;
+	double vpnx;
+	double vpny;
+	double vpnz;
+	double vupx;
+	double vupy;
+	double vupz;
+	double near;
+	double far;
+	
+	public Camera(String NAME, double PRPX, double PRPY, double PRPZ, double VPNX, double VPNY, double VPNZ, double VUPX, double VUPY, double VUPZ, double NEAR, double FAR){
+		name = NAME;
+		prpx = PRPX;
+		prpy = PRPY;
+		prpz = PRPZ;
+		vpnx = VPNX;
+		vpny = VPNY;
+		vpnz = VPNZ;
+		vupx = VUPX;
+		vupy = VUPY;
+		vupz = VUPZ;
+		near = NEAR;
+		far = FAR;
+	}
 }
--- src/Face.java
+++ src/Face.java
@@ -1,4 +1,14 @@
-
 public class Face {
 
+	Vertex[] vertices = new Vertex[3];
+	Mtl mtl;
+	
+	public Face(Vertex[] VERTICES){
+		vertices = VERTICES;
+	}
+	
+	public Face(Vertex[] VERTICES, Mtl MTL){
+		vertices = VERTICES;
+		mtl = MTL;
+	}
 }
